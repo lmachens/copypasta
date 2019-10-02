7197bef2d9ccdc60f7ade52937bc24c046b6d84e
lmachens <leon.machens@googlemail.com>
2019-10-02 17:42:33 +0200

    add paste area

---
+++ src/components/PasteArea.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const TextArea = styled.textarea`
+  border: 5px solid #ab9df2;
+  border-radius: 15px;
+  box-shadow: 0px 5px 0px #b4b4b4;
+  min-height: 200px;
+  min-width: 500px;
+  padding: 20px;
+`;
+
+export default function PasteArea() {
+  return <TextArea placeholder="text goes here..." autoFocus />;
+}
---
+++ src/stories/TextArea.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PasteArea from "../components/PasteArea";
+
+export default {
+  title: "PasteArea"
+};
+
+export const Default = () => <PasteArea />;
