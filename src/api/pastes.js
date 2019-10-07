75eb08070f0b5091c4695801e1064ab5ba88f2e0
lmachens <leon.machens@googlemail.com>
2019-10-07 09:34:06 +0200

    revert fix return of undefined

--- src/api/pastes.js
+++ src/api/pastes.js
@@ -8,7 +8,6 @@ export function getPaste(pasteId) {
       if (response.status !== 200) {
         throw new Error(response.statusText);
       }
-      return response;
     })
     .then(response => response.json());
 }
