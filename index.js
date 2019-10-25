cc285ebd64e6e7ee458af56fc7eb9558077e784b
Leon Machens <leon.machens@googlemail.com>
2019-10-25 20:09:16 +0200

    make database script more generic

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getCollection } = require("./lib/database");
+const { getCollection, initDatabase } = require("./lib/database");
 const { ObjectID } = require("mongodb");
 const path = require("path");
 
 const app = express();
 const port = 8080;
+const dbName = "copy-pasta";
+const collectionName = "pastes";
 
 // Parse application/json for all request
 app.use(express.json());
@@ -40,17 +42,22 @@ if (process.env.NODE_ENV === "production") {
 }
 
 async function set(value) {
-  const pastesCollection = await getCollection();
+  const pastesCollection = await getCollection(collectionName);
   const result = await pastesCollection.insertOne({ paste: value });
   return result.insertedId.toHexString();
 }
 
 async function get(key) {
-  const pastesCollection = await getCollection();
+  const pastesCollection = await getCollection(collectionName);
   const pasteId = new ObjectID.createFromHexString(key);
   const paste = await pastesCollection.findOne({ _id: pasteId });
   return paste.paste;
 }
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+initDatabase(dbName).then(() => {
+  console.log(`Database ${dbName} is ready`);
+
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 });
