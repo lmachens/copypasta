2f928117acaee54bdcf3bd84e834a4daf9d4a4b7
Leon Machens <leon.machens@googlemail.com>
2019-10-25 22:35:44 +0200

    add now integration (#8)

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
 
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
