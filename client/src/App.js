94931be7b500331f4549d7112397e3d88ddad880
aruppert <aruppert@pm.me>
2019-12-06 14:04:50 +0100

    Add confirmation and it's state to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,29 @@ import GlobalStyles from "./GlobalStyles";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Paste from "./pages/Paste";
+import Confirmation from "./components/Confirmation";
 
 function App() {
+  const [showConfirmation, setShowConfirmation] = React.useState(false);
+
+  function isPasteCreator() {
+    setShowConfirmation(true);
+  }
+
   return (
     <div className="App">
       <GlobalStyles />
+      {showConfirmation && (
+        <Confirmation>
+          <div>🎉🎉🎉</div>URL copied to your clipboard!
+        </Confirmation>
+      )}
       <Router>
-        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/"
+          component={() => <Home isPasteCreator={isPasteCreator} />}
+        />
         <Route path="/:pasteId" component={Paste} />
       </Router>
     </div>
