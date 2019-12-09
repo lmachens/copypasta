e9934388d93c09a6567c4d4de7f0f2c9802599d8
lmachens <leon.machens@googlemail.com>
2019-12-09 09:52:15 +0100

    Remove unused ids

--- client/src/components/SelectTime.js
+++ client/src/components/SelectTime.js
@@ -14,24 +14,12 @@ export default function Selector({ onChange }) {
       <label>
         When should your pasta expire?
         <SelectBox onChange={onChange}>
-          <option id="1" value={0}>
-            never
-          </option>
-          <option id="5" value={60000}>
-            1 minute
-          </option>
-          <option id="6" value={180000}>
-            3 minute
-          </option>
-          <option id="2" value={600000}>
-            10 Minutes
-          </option>
-          <option id="3" value={3600000}>
-            1 Hour
-          </option>
-          <option id="4" value={21600000}>
-            6 Hours
-          </option>
+          <option value={0}>never</option>
+          <option value={60000}>1 minute</option>
+          <option value={180000}>3 minute</option>
+          <option value={600000}>10 Minutes</option>
+          <option value={3600000}>1 Hour</option>
+          <option value={21600000}>6 Hours</option>
         </SelectBox>
       </label>
     </div>
