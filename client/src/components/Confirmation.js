2aad888939e2bc25f6d82b8b6d1fd4906ef8da69
lmachens <leon.machens@googlemail.com>
2019-12-09 11:04:17 +0100

    Inherit styles from Alert component

--- client/src/components/Confirmation.js
+++ client/src/components/Confirmation.js
@@ -1,32 +1,8 @@
+import Alert from "./Alert";
 import styled from "styled-components";
-import { keyframes } from "styled-components";
 
-export const flyFromRight = keyframes`
-0% {
-  right: -500px;
-}
-
-10%, 90% {
-  right: 20px;
-}
-
-100% {
-  right: -500px;
-}
-`;
-
-const Confirmation = styled.div`
+const Confirmation = styled(Alert)`
   border: 5px solid #a9dc76;
-  border-radius: 15px;
-  box-shadow: 0px 5px 0px #b4b4b4;
-  margin: 10px;
-  padding: 20px;
-  text-align: center;
-  position: fixed;
-  right: -500px;
-  top: 20px;
-  background: #fff;
-  animation: ${flyFromRight} 5s ease-in-out;
 `;
 
 export default Confirmation;
