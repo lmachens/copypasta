23727a50e08d3db7287b1c54c7f2112ea58f1d60
phlgr <philippm.gartz@gmail.com>
2020-03-27 13:59:23 +0100

    Add Alert on button press

--- client/src/components/EmbedButton.js
+++ client/src/components/EmbedButton.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Button from './Button';
+import Alert from './Alert';
 
 export default function EmbedButton(props) {
+  const [embedCodeCopied, setEmbedCodeCopied] = React.useState(false);
+  function handleEmbedButtonClick() {
+    setEmbedCodeCopied(true);
+
+    return;
+  }
   return (
     <>
-      <Button {...props}>Copy Embed Code</Button>
+      {embedCodeCopied && <Alert>Copied Embed Code to your Clipboard!</Alert>}
+      <Button {...props} onClick={() => handleEmbedButtonClick()}>
+        Copy Embed Code
+      </Button>
     </>
   );
 }
