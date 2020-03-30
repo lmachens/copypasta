bcf11d569f18d737c5da84a63913018d3f49c367
florianGierlichs <gierlichs@gmx.de>
2020-03-30 09:54:25 +0200

    Update slack bot link

--- client/src/api/slackConnectData.js
+++ client/src/api/slackConnectData.js
@@ -1,5 +1,5 @@
 const SLACK_URL =
-  'https://hooks.slack.com/services/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';
+  'https://hooks.slack.com/services/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';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const api = `${proxy}${SLACK_URL}`;
 
