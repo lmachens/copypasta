163088e98812f8e447635c53fa9f678db107dc4a
nadinho <rockt2935@gmail.com>
2020-03-30 10:11:36 +0200

    delete comment

--- client/src/components/Approval.js
+++ client/src/components/Approval.js
@@ -2,10 +2,6 @@ import React from 'react';
 import Confirmation from './Confirmation';
 
 function Approval(approved) {
-  //   const [approval, setApproval] = React.useState(false);
-  //   React.useEffect(() => {
-  //     setApproval(true);
-  //   });
   if (approved === true) {
     return <Confirmation>Email Successfully Sent 🎁</Confirmation>;
   }
