e0984a5cefa4ab79e1425ebcfa1fb09a5c5a3f6d
Philipp <philippm.gartz@gmail.com>
2020-03-30 12:47:41 +0200

    Add Embed functionality (#28)
    * Embed Components
    
    * Add Components to pages
    
    * Add Button functionality
    
    * Add Alert on button press
    
    * Fixed prop types in EmbedButton
    
    * Fix problems with overflow text
    
    * Add copy function for iframe
    
    * Handle edgecase
    
    * Change color of Embed alert
    
    * Fix propTypes
    
    * Remove return in EmbedButton
    
    * Rename paste to pasta
    
    * Move default export to end of file
    
    * Create copyToClipboard helper
    
    * Remove additional arrow function
    
    * Use label and remove empty styled component
    
    * Reuse existing Paste page
    
    Co-authored-by: lenkly <lenk.lydia@gmail.com>
    Co-authored-by: lmachens <leon.machens@googlemail.com>

---
+++ client/src/components/EmbedButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from './Button';
+import Confirmation from './Confirmation';
+import PropTypes from 'prop-types';
+import { copyToClipboard } from '../utils/clipboard';
+
+function EmbedButton(props) {
+  const [embedCodeCopied, setEmbedCodeCopied] = React.useState(false);
+
+  function handleEmbedButtonClick() {
+    setEmbedCodeCopied(true);
+    const iframeCode = `<iframe src="${window.location.origin}/embed/${props.pasteId}"/>`;
+    copyToClipboard(iframeCode);
+  }
+
+  return (
+    <>
+      {embedCodeCopied && (
+        <Confirmation>Copied Embed Code to your Clipboard!</Confirmation>
+      )}
+      <Button {...props} onClick={handleEmbedButtonClick}>
+        Copy Embed Code
+      </Button>
+    </>
+  );
+}
+
+EmbedButton.propTypes = {
+  pasteId: PropTypes.string
+};
+
+export default EmbedButton;
--- client/src/pages/Paste.js
+++ client/src/pages/Paste.js
@@ -8,6 +8,7 @@ import Button from '../components/Button';
 import FullContainer from '../components/FullContainer';
 import Author from '../components/Author';
 import PropTypes from 'prop-types';
+import EmbedButton from '../components/EmbedButton';
 import PastaPoints from '../components/PastaPoints';
 
 const PasteArea = styled.div`
@@ -18,7 +19,14 @@ const CreatedAt = styled(DateTime)`
   margin: 10px;
 `;
 
-function Paste({ match }) {
+const Content = styled.div`
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+function Paste({ match, embedded }) {
   const { pasteId } = match.params;
   const [{ paste, error, loading }, doGet] = useGetPaste(pasteId);
 
@@ -34,21 +42,23 @@ function Paste({ match }) {
         </>
       )}
       {paste && (
-        <>
+        <Content>
           <CreatedAt date={new Date(paste.createdAt)}>
             {new Date(paste.createdAt).toDateString()}
           </CreatedAt>
           <Author name={paste.author} />
           <PastaPoints pastaPoints={paste.pastaPoints} pasteId={pasteId} />
           <PasteArea>{paste.value}</PasteArea>
-        </>
+          {!embedded && paste.isEmbeddable && <EmbedButton pasteId={pasteId} />}
+        </Content>
       )}
     </FullContainer>
   );
 }
 
 Paste.propTypes = {
-  match: PropTypes.object
+  match: PropTypes.object,
+  embedded: PropTypes.bool
 };
 
 export default Paste;
