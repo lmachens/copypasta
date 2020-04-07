63f106129d55d43f03b6b63eb0e375d7f75d72df
lmachens <leon.machens@googlemail.com>
2020-04-07 16:54:11 +0200

    Fix import of non existing file

--- client/src/stories/Buttons.stories.js
+++ client/src/stories/Buttons.stories.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import SubmitButton from '../components/SubmitButton';
-import VoteButton from '../components/VoteButton';
 
 export default {
   title: 'Buttons',
 };
 
 export const Submit = () => <SubmitButton />;
-export const Vote = () => <VoteButton />;
