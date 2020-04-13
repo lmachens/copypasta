8047057b0e6b119c2ffc51c8ada69270fda7e3ad
marwinburesch <marwin@posteo.de>
2020-04-13 17:21:39 +0200

    Adjust Button to relative font-size and height

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -4,9 +4,10 @@ const Button = styled.button`
   background: #a9dc76;
   box-shadow: 0px 5px 0px #b4b4b4;
   border-radius: 15px;
-  font-size: 22px;
+  font-size: 1.2rem;
   color: #505050;
   padding: 4px 15px;
+  height: fit-content;
   cursor: pointer;
 
   &:disabled {
