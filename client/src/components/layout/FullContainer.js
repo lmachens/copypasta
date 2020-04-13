9704863eb6cdf9e89eda6223e3e2c596d61873ef
marwinburesch <marwin@posteo.de>
2020-04-13 17:13:54 +0200

    Adjust FullContainer flexbox behaviour

--- client/src/components/layout/FullContainer.js
+++ client/src/components/layout/FullContainer.js
@@ -2,11 +2,16 @@ import styled from '@emotion/styled';
 
 const FullContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-flow: row wrap;
+  align-items: stretch;
   justify-content: center;
   min-height: 100vh;
+  max-width: 850px;
   padding: 20px;
+  margin: auto;
+  & > div {
+    margin-top: 20px;
+  }
 `;
 
 export default FullContainer;
