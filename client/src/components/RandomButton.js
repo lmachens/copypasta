9ecbf3a75f02e536594afc0d8d68f12ab8f1dcb6
marwinburesch <marwin@posteo.de>
2020-04-14 14:02:50 +0200

    Remove random pasta emoji

--- client/src/components/RandomButton.js
+++ client/src/components/RandomButton.js
@@ -2,12 +2,5 @@ import React from 'react';
 import Button from './Button';
 
 export default function RandomButton(props) {
-  return (
-    <Button {...props}>
-      get random pasta{' '}
-      <span role="img" aria-label="random-emoji">
-        🍝
-      </span>
-    </Button>
-  );
+  return <Button {...props}>get random pasta</Button>;
 }
